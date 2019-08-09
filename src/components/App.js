ae58d2931961c14a74c983768ad7ef9a46868648
Alex Votry <alexvotry@gmail.com>
2019-08-09 13:39:22 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,6 @@ class App extends Component {
     const response = await unsplash(term);
     this.setState({ images: response.data.results });
   }
-  //Access Key:  d8bd8bf84026edb300bc8f60cd9ae03cc9a60887a4582edffeb1a902c07912f7
-  //Secret Key: a9471f0ed9a3e280de2ba1f7fee28850ba73b454b68e98eab1dfff08c8b45526
 
   render() {
     return (
